35608530133d7530b95d1c68fc6ed90fad36d48b
Junior <74092633+juniorstz@users.noreply.github.com>
2021-04-13 02:17:53 -0300

    Delete Webflux directory

--- Webflux/src/main/java/com/apirest/webflux/repository/PlaylistRespository.java
+++
@@ -1,9 +0,0 @@
-package com.apirest.webflux.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.apirest.webflux.document.Playlist;
-
-public interface PlaylistRespository extends ReactiveMongoRepository<Playlist, String>{
-
-}
